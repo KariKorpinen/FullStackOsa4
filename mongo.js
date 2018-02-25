67873ac1182e95effd03e97d8da3a7e3e5c7ce76
KariKorpinen <karikkorpinen@gmail.com>
2018-02-25 23:52:13 +0200

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 
-//const url = 'mongodb://fullstack:fullstack@ds211088.mlab.com:11088/fullstack-notes'
-const url = 'mongodb://blogiuser:mail30@ds249128.mlab.com:49128/blogi'
-//const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 mongoose.Promise = global.Promise
